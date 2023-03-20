1844b1c24e39425985b70c16e6cfb21899f8f61e
zchhhhh <2069820192@qq.com>
2023-03-20 17:41:42 +0800

    2023.3.20

---
+++ Vue-CLI/02_project_vite/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>App</div>
+    <h2>{{ price }}</h2>
+    <app-header></app-header>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+    name: "App",
+    data() {
+      return {
+        price: 999
+      }
+    },
+    components: {
+      AppHeader
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ Vue-CLI/02_project_vite/src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>AppHeader</div>
+    <h1>我是AppHeader</h1>
+</template>
+
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<style scoped>
+    
+</style>
