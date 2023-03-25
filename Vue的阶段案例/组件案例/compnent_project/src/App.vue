482a23c1dc55af16ff06e3ba8ef4630bd0b6aeb8
zchhhhh <2069820192@qq.com>
2023-03-25 16:21:20 +0800

    增加Vue-组件，以及一个组件案例

---
+++ Vue的阶段案例/组件案例/compnent_project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <tab-control :titles="['衣服', '鞋子', '包包']" @tab-item-click="tabClick">
+    </tab-control>
+    <!-- 展示的内容 -->
+    <h1>{{ products[currentIndex] }}</h1>
+  </div>
+</template>
+
+<script>
+
+import TabControl from './component/TabControl.vue';
+
+export default {
+  components: {
+    TabControl
+  },
+  data() {
+    return {
+      currentIndex: -1,
+      products: ["衣服页面", "鞋子页面", "包包页面"]
+    }
+  },
+  methods: {
+    tabClick(index) {
+      this.currentIndex = index
+    }
+  },
+}
+</script>
+
+<style scoped></style>
