7804bc30932210a1b7e8e046b55918098b0a7df2
zchhhhh <2069820192@qq.com>
2023-10-10 11:24:02 +0800

    feat：新增React脚手架的安装及基础使用

---
+++ 02_learn_scaffold/src/Components/HelloWorld.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>我是h2</h2>
+        <p>Hello, 我是HelloWorld组件</p>
+      </div>
+    )
+  }
+}
+
+export default HelloWorld
